90698ace886762792c7bf85d7c1c4a219b1380c0
RB <82284311+Roman123-game@users.noreply.github.com>
2025-03-08 11:55:03 +0200

    create comment

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import ObjectDetection from './components/ObjectDetection/ObjectDetection';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Image Object Detection</h1>
+        <ObjectDetection />
+    </div>
+  );
+}
+
+export default App;
